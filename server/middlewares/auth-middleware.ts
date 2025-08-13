53ad1b14c6f95091fb201b8e4bb81235ce9b2542
Mihailo Boskovic <mihailo.boskovic@coop.co.uk>
2025-08-13 15:05:38 +0200

    codegames-be #8: we have auth middleware for protected routes

---
+++ server/middlewares/auth-middleware.ts
@@ -0,0 +1,60 @@
+import { Request, Response, NextFunction } from 'express';
+import { HttpError } from '../types/common/error-types';
+import jwt from 'jsonwebtoken';
+import { JwtPayloadType } from '../types/dto/user-types';
+export interface AuthRequest extends Request {
+	token?: string;
+}
+
+export const authMiddleware = (
+	req: AuthRequest,
+	res: Response,
+	next: NextFunction
+) => {
+	const token = extractTokenFromRequest(req);
+
+	if (!token) {
+		return next(new HttpError(401, 'Unauthorized'));
+	}
+
+	const secret = process.env.JWT_SECRET;
+
+	if (!secret) {
+		return next(new HttpError(500, 'JWT_SECRET is not defined'));
+	}
+
+	try {
+		const decoded = jwt.verify(token, secret) as JwtPayloadType;
+
+		if (!decoded) {
+			return next(new HttpError(401, 'Unauthorized'));
+		}
+
+		req.token = token;
+		return next();
+	} catch (error) {
+		if (error instanceof jwt.JsonWebTokenError) {
+			return next(new HttpError(401, 'Invalid token'));
+		}
+
+		if (error instanceof jwt.TokenExpiredError) {
+			return next(new HttpError(401, 'Token expired'));
+		}
+
+		if (error instanceof jwt.NotBeforeError) {
+			return next(new HttpError(401, 'Token not active'));
+		}
+
+		return next(new HttpError(500, 'Authentication error'));
+	}
+};
+
+const extractTokenFromRequest = (request: Request): string | null => {
+	const authorizationHeader = request.headers.authorization;
+
+	if (authorizationHeader && authorizationHeader.startsWith('Bearer ')) {
+		return authorizationHeader.substring(7);
+	}
+
+	return request.cookies?.token ?? null;
+};
