e7b8267dff50195cc7fc5196ead24b73b6a00975
Mihailo Boskovic <mihailo.boskovic@coop.co.uk>
2026-02-19 23:35:01 +0100

    total redesign

--- server/routes/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as authRouter } from "./auth-route";
-export { default as MainRouter } from "./main-router";
-export { default as adminRouter } from "./admin-route";
-export { default as codeExecutionRouter } from "./code-route";
-export { healthRouter } from "./health-router";
